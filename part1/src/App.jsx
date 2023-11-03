de1c94df0a94aea2177078cd594b21988bc8b3e0
micapremat <micapremat@gmail.com>
2023-11-03 11:44:56 +0100

    part1 - exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return(
     <div>
       <p>
@@ -36,9 +36,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
       <Total exercises={exercises1 + exercises2 + exercises3}/>
     </div>
   )
